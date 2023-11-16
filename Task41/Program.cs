721bab66270554f755aeb25b8dec6b51174d50bc
zhyk <zhyk@list.ru>
2023-11-16 11:40:24 +0300

    Задача № 41

---
+++ Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Write("Введите кол-во элементов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int number = 0;
+for (int i = 1; i <= m; i++)
+{
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во положительных элементов равно {count}");
+
+
